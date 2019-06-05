c2579176cc4fb727c626e7f6de77c7eda08a79c1
Weipeng Zheng <dastonzerg93@gmail.com>
2019-06-04 22:08:17 -0700

    before admin_user

--- spark_gathering/app/models/order.rb
+++ spark_gathering/app/models/order.rb
@@ -3,4 +3,11 @@ class Order < ApplicationRecord
   PAYMENT_TYPES = ["Check", "Credit Card", "Venmo", "Paypal"]
   validates :name, :address, :email, presence: true
   validates :paytype, inclusion: PAYMENT_TYPES
+
+  def add_items_from_cart(cart)
+    cart.lineitems.each do |item|
+      item.cart_id = nil
+      item.order_id = self.id
+    end
+  end    
 end
