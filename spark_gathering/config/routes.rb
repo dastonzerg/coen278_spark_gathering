59021810f86f356cd7b2cfabd3fcc480cbcfe072
Weipeng Zheng <dastonzerg93@gmail.com>
2019-06-09 15:35:48 -0700

    logout done

--- spark_gathering/config/routes.rb
+++ spark_gathering/config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'access/new'
+  get 'access/create'
+  get 'access/destroy'
+  get 'admin/index'
   resources :users
   resources :orders
   resources :lineitems
@@ -7,5 +11,9 @@ Rails.application.routes.draw do
   get 'shopper/index'
   resources :products
   root :to=>'home#index'
+  get 'admin', to: 'admin#index'
+  get 'login', to: 'access#new'
+  get 'logout', to: 'access#destroy'
+  post '/access/new', to: 'access#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
